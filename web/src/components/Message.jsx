3c34c0985c991b25ff14c0efb6639907bbb9c8ce
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-05 01:15:06 +0100

    mvp: markdown works

--- web/src/components/Message.jsx
+++ web/src/components/Message.jsx
@@ -1,10 +1,13 @@
 import './Message.css'
+import ReactMarkdown from 'react-markdown'
 
 const Message = ({ message }) => {
   const { role, content } = message
 
   return (
-    <div className={`message message-role-${role}`}>{content}</div>
+    <div className={`message message-role-${role}`}>
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </div>
   )
 }
 
