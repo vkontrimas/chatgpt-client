c262c4ceb2c4463db21854f7a5da5203d45e9d72
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-08 13:00:35 +0100

    web: refactor message view into Messages comp

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,31 +1,12 @@
-import { useEffect, useRef } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-
 import MessageInput from './components/MessageInput'
-import Message from './components/Message'
-import { fetchAll } from './redux/message'
+import Messages from './components/Messages'
 
 import './App.css'
 
 const App = () => {
-  const messages = useSelector(state => state.message.messages)
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch(fetchAll())
-  }, [])
-
-  const messagesRef = useRef(null)
-  useEffect(() => {
-    messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
-  }, [messages])
-
   return (
     <div className="App">
-      <div className="messages" ref={messagesRef}>
-        {messages.map((message, i) => <Message key={i} message={message}/>)}
-      </div>
-
+      <Messages />
       <MessageInput />
     </div>
   )
