2e06b81b2125f8d86d03b5ea979782c0cf37da22
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-08 20:11:57 +0100

    Set up react router

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,15 +1,26 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider as ReduxProvider } from 'react-redux'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
 
 import { store } from './redux/store.js'
 import App from './App'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ReduxProvider store={store}>
     <StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </StrictMode>
   </ReduxProvider>,
 )
