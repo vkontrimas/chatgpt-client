0ce74c833fb41684783662beb3f4b042cf6e50cd
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-09 14:49:56 +0100

    add user router

--- back/src/app.js
+++ back/src/app.js
@@ -4,11 +4,13 @@ const cors = require('cors')
 const unknownEndpoint = require('./mid/unknown_endpoint')
 const errorHandler = require('./mid/error_handler')
 const messageRouter = require('./api/message')
+const usersRouter = require('./api/users')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api/message', messageRouter)
+app.use('/api/users', usersRouter)
 app.use(unknownEndpoint)
 app.use(errorHandler)
 module.exports = app
