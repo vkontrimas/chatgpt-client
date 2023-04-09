071be654cd67c3127063b5be9faf3b3548398db1
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-09 15:00:44 +0100

    add /api/users endpoint

--- back/src/api/users.js
+++ back/src/api/users.js
@@ -1,5 +1,34 @@
 const usersRouter = require('express').Router()
 
+const User = require('../db/user')
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.findAll()
+  const result = users.map(user => ({
+    id: user.id,
+    email: user.email,
+  }))
+  console.log(users, result)
+  response.status(200).json(result)
+})
+
+usersRouter.post('/', async (request, response) => {
+  const newUser = request.body
+
+  if (!newUser.email) {
+    return response.status(400).json({ error: 'no email' })
+  }
+  if (!newUser.password) {
+    return response.status(400).json({ error: 'no password' })
+  }
+
+  const passwordHash = 'todo'
+
+  const user = await User.create({ email: newUser.email, passwordHash, })
+  response.status(201).json({
+    id: user.id,
+    email: user.email,
+  })
+})
 
 module.exports = usersRouter
---
+++ back/src/tests/api/users.test.js
@@ -0,0 +1,85 @@
+const supertest = require('supertest')
+const api = supertest(require('../../app'))
+
+const ENDPOINT = '/api/users'
+
+const { 
+  modelUser,
+  initialUsers,
+  initializeDB,
+  wipeDB,
+  fetchAllUsers,
+} = require('../db_helper')
+
+describe(`API ${ENDPOINT}`, () => {
+  beforeEach(async () => {
+    await initializeDB()
+  })
+
+  afterEach(async () => {
+    await wipeDB()
+  })
+
+  test('GET - 200 - returns all users', async () => {
+    const response = await api
+      .get(ENDPOINT)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const users = response.body
+
+    expect(initialUsers.length).toBe(users.length)
+    expect(initialUsers.map(user => user.email)).toMatchObject(users.map(user => user.email))
+  })
+
+  test('POST - no email - 400 error', async () => {
+    const request = {
+      ...modelUser(),
+      email: undefined,
+    }
+
+    const response = await api
+      .post(ENDPOINT)
+      .send(request)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.error).toMatch(/no email/)
+  })
+
+  test('POST - no password - 400 error', async () => {
+    const request = {
+      ...modelUser(),
+      password: undefined,
+    }
+
+    const response = await api
+      .post(ENDPOINT)
+      .send(request)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.error).toMatch(/no password/)
+  })
+
+  test('POST - valid - 201 created', async () => {
+    const request = modelUser()
+
+    const response = await api
+      .post(ENDPOINT)
+      .send(request)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const user = response.body
+    console.log(user)
+    expect(user.email).toBe(request.email)
+    expect(user.passwordHash).not.toBeDefined()
+    expect(user.id).toBeDefined()
+
+    const users = await fetchAllUsers()
+    expect(users.length).toBe(initialUsers.length + 1)
+    expect(users.map(user => user.email)).toContain(request.email)
+  })
+})
+
