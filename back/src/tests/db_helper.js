bf1aeaeccc5e1cba39f7492933be12062c0da001
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-09 14:44:09 +0100

    init db with users for test

--- back/src/tests/db_helper.js
+++ back/src/tests/db_helper.js
@@ -1,8 +1,19 @@
 const sequelize = require('../db/sequelize')
 const User = require('../db/user')
 
+const initialUsers = [
+  {
+    email: 'test@example.com',
+  },
+  {
+    email: 'test2@example.com',
+  },
+]
+
 const initializeDB = async () => {
+  await sequelize.drop()
   await sequelize.sync()
+  await Promise.all(initialUsers.map(user => User.create(user)))
 }
 
 const wipeDB = async () => {
@@ -10,6 +21,7 @@ const wipeDB = async () => {
 }
 
 module.exports = {
+  initialUsers,
   initializeDB,
-  wipeDB
+  wipeDB,
 }
