b817eb5a3cc20352f653f806234c2b250ef39142
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-10 23:34:48 +0100

    delete chat and message

--- back/src/db/chat.js
+++
@@ -1,8 +0,0 @@
-const { DataTypes } = require('sequelize')
-
-module.exports = (sql) => sql.define('Chat', {
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-})
--- back/src/db/db.js
+++ back/src/db/db.js
@@ -2,8 +2,6 @@ const { Sequelize } = require('sequelize')
 const { DB_PATH, DB_LOG } = require('../config')
 
 const initializeUser = require('./user')
-const initializeChat = require('./chat')
-const initializeMessage = require('./message')
 
 const initializeDb = () => {
   const sequelize = new Sequelize(DB_PATH, {
@@ -11,25 +9,12 @@ const initializeDb = () => {
   })
 
   const User = initializeUser(sequelize)
-  const Chat = initializeChat(sequelize)
-  const Message = initializeMessage(sequelize)
-
-  User.hasMany(Chat)
-  Chat.belongsTo(User)
-
-  Chat.hasMany(Message)
-  Message.belongsTo(Chat)
-
-  User.hasMany(Message)
-  Message.belongsTo(User)
 
   sequelize.sync() // TODO: remove this
 
   return {
     sequelize,
     User,
-    Chat,
-    Message
   }
 }
 
