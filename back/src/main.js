5a9a542e684668a7ada01b9f3ae2461498257395
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-10 20:56:51 +0100

    HACK: create test user while I'm on the tube

--- back/src/main.js
+++ back/src/main.js
@@ -7,4 +7,17 @@ const User = require('./db/user')
 // TODO: remove this when we switch from in-memory
 sequelize.sync()
 
+const bcrypt = require('bcrypt')
+
+const { PASSWORD_HASH_ROUNDS } = require('./config')
+
+const createDevAccount = async () => {
+  const user = await User.create({
+    email: 'vk@example.com',
+    passwordHash: await bcrypt.hash('sekret', PASSWORD_HASH_ROUNDS),
+  })
+  console.log(user)
+}
+createDevAccount()
+
 app.listen(PORT, () => { console.log(`Server listening on ${PORT}`)})
