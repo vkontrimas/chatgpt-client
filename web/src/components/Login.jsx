31937dd7bb46906b107c3bc3cc5f2dafeceba83e
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-10 15:02:15 +0100

    handle login from login component

--- web/src/components/Login.jsx
+++ web/src/components/Login.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { login } from '../redux/user'
+
+import { useNavigate } from 'react-router'
 
 import './Login.css'
 
@@ -9,10 +12,28 @@ const Auth = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const userState = useSelector(state => state.user)
 
-  const handleSubmit = (event) => {
+  useEffect(() => {
+    // If logged in, go to chat
+    if (userState.token) {
+      navigate('/chat')
+    }
+  }, [userState])
+
+  const handleSubmit = async (event) => {
     event.preventDefault()
-    dispatch(login({ email, password }))
+    try {
+      const response = await axios.post('/api/login', { email, password })
+      dispatch(login(response))
+    } catch (error) {
+      if (error.name !== 'AxiosError') {
+        throw error
+      }
+
+      console.error('login failed\n', error)
+    }
   }
 
   return (
