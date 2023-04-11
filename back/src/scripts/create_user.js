90706e0ffe8cec2a56eea30631fa55c82a3da31e
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-11 11:45:36 +0100

    add script to create user

---
+++ back/src/scripts/create_user.js
@@ -0,0 +1,40 @@
+const inquirer = require('inquirer')
+const bcrypt = require('bcrypt')
+
+const { User } = require('../db/db')
+const { PASSWORD_HASH_ROUNDS } = require('../config')
+
+const run = async () => {
+  const { email, password } = await inquirer.prompt([
+    {
+      name: 'email',
+      validate: (input) => {
+        if (!/.+@.+\..+/.test(input)) {
+          return "doesn't look like a valid email"
+        }
+        return true
+      },
+      message: 'Email address:',
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Password:',
+    }
+  ])
+
+  const passwordHash = await bcrypt.hash(password, PASSWORD_HASH_ROUNDS)
+
+  try {
+    const user = await User.create({ email, passwordHash, })
+    console.log('Created user!')
+  } catch (error) {
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      console.error('email in use')
+    } else {
+      throw error
+    }
+  }
+}
+
+run()
