92e6d757d0688766dcb4ff71207efc25a9be923f
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-12 14:21:12 +0100

    Always print command

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 run_cmd() {
+  echo "\$ $@"
   if [ -z "$DRY_RUN" ]; then
     eval "$@"
-  else
-    echo "$@"
   fi
 }
 
 run_cmd cd 'web'
-# we need to install vite to build 
-run_cmd npm install --save-dev 
+# we need to install vite to build, hence NODE_ENV
+run_cmd NODE_ENV=development npm install
 run_cmd npm run build -- --outDir '../back/public'
 run_cmd rm -rf node_modules/
 run_cmd cd '../back'
