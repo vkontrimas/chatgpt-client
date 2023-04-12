4e3178a1c280e92b8aaed08491ba14a73ac9b565
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-12 02:53:53 +0100

    Add clear option

--- web/src/components/User.jsx
+++ web/src/components/User.jsx
@@ -7,12 +7,20 @@ import './User.css'
 const User = () => {
   const dispatch = useDispatch()
 
+  const handleLogout = () => dispatch(logout())
+  const handleClear = () => {}
+
   return (
     <div className="user-vertical">
       <div className="user-name">Vytautas</div>
-      <button className="user-button" onClick={() => dispatch(logout())}>
-        Log out
-      </button>
+      <div className="button-row">
+        <button className="user-button" onClick={handleLogout}>
+          Exit
+        </button>
+        <button className="user-button" onClick={handleClear}>
+          Clear
+        </button>
+      </div>
     </div>
   )
 }
