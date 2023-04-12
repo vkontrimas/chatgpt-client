922b3bf0d21898364c7050d2fada68cf3e76c5cb
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-13 00:02:26 +0100

    simplify autofocus

--- web/src/components/ChatInput.jsx
+++ web/src/components/ChatInput.jsx
@@ -6,24 +6,17 @@ import './ChatInput.css'
 const ChatInput = ({ enabled }) => {
   const dispatch = useDispatch()
   const [message, setMessage] = useState('')
+  const inputRef = useRef(null)
 
   const handleSubmit = async (event) => {
     event?.preventDefault()
     if (message) {
-      setMessage('')
       // TODO: Find out if the markdownified message significantly affects
       //       GPT replies. (Also definitely should add tokens? Maybe not?)
       dispatch(create({ type: 'user', content: message.replace(/\n/g, '  \n') }))
+      setMessage('')
     }
   }
-
-  const inputRef = useRef(null)
-  const focusInput = () => inputRef.current?.focus()
-  useEffect(() => { if (enabled) { focusInput() } }, [enabled])
-  useEffect(() => {
-    window.addEventListener('focus', focusInput)
-    return () => window.removeEventListener('focus', focusInput)
-  }, [])
 
   const autoSize = (target) => {
     target.style.height = '0px'
@@ -54,6 +47,7 @@ const ChatInput = ({ enabled }) => {
         onChange={handleChange}
         ref={inputRef}
         disabled={!enabled}
+        autoFocus
       />
       <button className="send-button" action="submit" disabled={!enabled}>{'send'}</button>
     </form>
