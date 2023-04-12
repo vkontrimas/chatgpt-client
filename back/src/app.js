c7aff718f18d586752926b0e9998372ad2e84c7e
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-12 13:58:38 +0100

    I don't think we need cors in prod?

--- back/src/app.js
+++ back/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 require('express-async-errors')
-const cors = require('cors')
 const unknownEndpoint = require('./mid/unknown_endpoint')
 const errorHandler = require('./mid/error_handler')
 const userToken = require('./mid/userToken')
@@ -15,7 +14,10 @@ if (ENVIRONMENT !== 'test') {
   const morgan = require('morgan')
   app.use(morgan('tiny'))
 }
-app.use(cors())
+if (ENVIRONMENT === 'development' || environment === 'test') {
+  const cors = require('cors')
+  app.use(cors())
+}
 app.use(express.json())
 app.use('/api/message', userToken, messageRouter)
 app.use('/api/users', usersRouter)
