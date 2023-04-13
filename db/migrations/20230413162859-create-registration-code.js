8fe1f8a2f4a9735a3635cf43bb1b6d4ac2ac614e
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-13 17:29:50 +0100

    add registration code model

---
+++ db/migrations/20230413162859-create-registration-code.js
@@ -0,0 +1,29 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('RegistrationCodes', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      remainingUses: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('RegistrationCodes');
+  }
+};
