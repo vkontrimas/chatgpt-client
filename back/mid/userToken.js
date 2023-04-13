c32004ccc694478a19f93219fa2701c884100a79
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-13 23:51:05 +0100

    Delete userToken middleware

--- back/mid/userToken.js
+++
@@ -1,9 +0,0 @@
-const userToken = (request, response, next) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    request.userToken = authorization.replace('Bearer ', '')
-  }
-  next()
-}
-
-module.exports = userToken
