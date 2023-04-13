9fa68d759cdeba68eaa86209af72d9e641b0e1c0
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-13 16:35:25 +0100

    no longer sequelize sync

--- back/main.js
+++ back/main.js
@@ -1,7 +1,4 @@
 const app = require('./app')
 const { PORT } = require('./config')
-const { sequelize } = require('./db/db')
 
-sequelize.sync().then(() => {
-  app.listen(PORT, () => { console.log(`Server listening on ${PORT}`)})
-})
+app.listen(PORT, () => { console.log(`Server listening on ${PORT}`)})
