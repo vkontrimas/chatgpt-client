69981e851a551a6ba5943f541f8b00382363d5fc
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-13 16:11:03 +0100

    only create user when starting devdb

--- devdb/docker_scripts/0-init.sql
+++ devdb/docker_scripts/0-init.sql
@@ -1,13 +1,1 @@
-CREATE TABLE note (
-  id SERIAL NOT NULL,
-  name TEXT UNIQUE NOT NULL,
-  content TEXT NOT NULL
-);
-
 CREATE USER huddle WITH PASSWORD 'huddle';
-CREATE DATABASE huddle_development;
-CREATE DATABASE huddle_test;
-GRANT ALL PRIVILEGES ON DATABASE huddle_development TO huddle;
-GRANT ALL PRIVILEGES ON DATABASE huddle_test TO huddle;
-ALTER DATABASE huddle_development OWNER TO huddle;
-ALTER DATABASE huddle_test OWNER TO huddle;
