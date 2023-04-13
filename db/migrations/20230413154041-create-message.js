606abcc718a976383fc921df10e9fe240d13654c
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-13 16:41:14 +0100

    add Chat and Message models

---
+++ db/migrations/20230413154041-create-message.js
@@ -0,0 +1,32 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Messages');
+  }
+};
