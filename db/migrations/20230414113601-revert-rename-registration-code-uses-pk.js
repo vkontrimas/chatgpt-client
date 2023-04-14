f7c170cfbf91045b24648a1714290c2cb3e025e7
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-14 12:37:35 +0100

    Revert registration code uses pk rename

---
+++ db/migrations/20230414113601-revert-rename-registration-code-uses-pk.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.renameColumn('RegistrationCodeUses', 'UserId', 'id')
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.renameColumn('RegistrationCodeUses', 'id', 'UserId')
+  }
+};
