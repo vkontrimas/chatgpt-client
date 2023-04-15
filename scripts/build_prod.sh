898747afe22c9695dfcac07884d548d363808f60
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-15 10:54:07 +0100

    don't remove db/node_modules on deploy

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -15,6 +15,5 @@ run_cmd rm -rf node_modules/
 run_cmd cd '../db'
 run_cmd npm install --include dev
 run_cmd npx sequelize-cli db:migrate
-run_cmd rm -rf node_modules/
 run_cmd cd '../back'
 run_cmd npm install
