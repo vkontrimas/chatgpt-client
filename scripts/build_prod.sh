6c6908d8e317f379d02647cb7f750d9f7d40a9ed
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-15 10:52:16 +0100

    migrate db on deploy

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -12,5 +12,9 @@ run_cmd cd 'web'
 run_cmd npm install --include dev
 run_cmd npm run build -- --outDir '../back/public'
 run_cmd rm -rf node_modules/
+run_cmd cd '../db'
+run_cmd npm install --include dev
+run_cmd npx sequelize-cli db:migrate
+run_cmd rm -rf node_modules/
 run_cmd cd '../back'
 run_cmd npm install
