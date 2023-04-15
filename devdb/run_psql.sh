ae7f3b9c04317654394bd3fbec9dfa72bf367fe4
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-15 11:16:20 +0100

    make it easier to connect to prod db from psql

--- devdb/run_psql.sh
+++ devdb/run_psql.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-database='dev'
-if [[ -n "$1" ]]; then
-  database="$1"
+source back/.env
+
+if [[ "$1" == "prod" ]]; then
+  if [[ -n "$HUDDLE_PROD_DATABASE_URL" ]]; then
+    echo "Connecting to prod database"
+    echo "==========================="
+    psql $HUDDLE_PROD_DATABASE_URL
+  else
+    echo "HUDDLE_PROD_DATABASE_URL undefined! export it or add it to back/.env!"
+  fi
+else 
+  db_username='postgres'
+  db_name='huddle_dev'
+  db_hostname='localhost'
+  db_port=3003
+  db_pass='huddle'
+  db_name='huddle_dev'
+
+  if [[ -n "$1" ]]; then
+    db_name="huddle_$1"
+  fi
+  
+  echo "Connecting to ($db_name@$db_hostname) database..."
+  echo "================================================="
+  PGPASSWORD="$db_pass" psql -U $db_username -d $db_name -h $db_hostname -p $db_port
 fi
 
-echo "Connecting to $database database"
-psql -U postgres -d "huddle_$database" -h localhost -p 3003
