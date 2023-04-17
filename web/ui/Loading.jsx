b1d46f4017bcedf483aa23138fa13222faa8a31b
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-17 20:48:57 +0100

    Make loading screen prettier

--- web/ui/Loading.jsx
+++ web/ui/Loading.jsx
@@ -3,7 +3,7 @@ import '../css/Loading.css'
 const Loading = () => {
   return (
     <div className='loading'>
-      <i className='fa fa-cycle fa-spinner fa-2x' />
+      <i className='fa fa-pulse fa-spinner fa-3x' />
     </div>
   )
 }
