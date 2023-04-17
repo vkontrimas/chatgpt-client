4382dbb7c4491ea506a1f0e656a7fe4befb5f0eb
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-17 23:50:56 +0100

    Only show loading circle if request takes more than 150ms

--- web/ui/Loading.jsx
+++ web/ui/Loading.jsx
@@ -1,11 +1,24 @@
 import '../css/Loading.css'
 
+import { useState, useEffect } from 'react'
+
 const Loading = () => {
-  return (
-    <div className='loading'>
-      <i className='fa fa-pulse fa-spinner fa-3x' />
-    </div>
-  )
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    const timer = setTimeout(() => { setShow(true) }, 150)
+    return () => { clearTimeout(timer) }
+  }, [])
+
+  if (show) {
+    return (
+      <div className='loading'>
+        <i className='fa fa-pulse fa-spinner fa-3x' />
+      </div>
+    )
+  }
+  else {
+    return null
+  }
 }
 
 export default Loading
