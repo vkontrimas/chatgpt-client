8dac71a8782d858dc3b8daa178ca62bab18e1aff
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-17 07:11:33 +0100

    Add to panel

--- web/ui/Main.jsx
+++ web/ui/Main.jsx
@@ -1,11 +1,12 @@
 import '../css/Main.css'
 
 import UserPanel from './UserPanel'
+import TopBar from './TopBar'
 
 const Main = () => {
   return (
     <div className='main'>
-      <div className='main-topbar'></div>
+      <div className='main-topbar'><TopBar /></div>
       <div className='main-sidebar'></div>
       <div className='main-sidebar-tab'></div>
       <div className='main-content'></div>
---
+++ web/ui/TopBar.jsx
@@ -0,0 +1,18 @@
+import '../css/TopBar.css'
+
+const TopBar = () => {
+  return (
+    <div className='top-bar'>
+      <h3 className='top-bar-title'>Untitled chat</h3>
+      <button
+        className='button2 top-bar-button'
+        onClick={() => {}}
+      >
+        Clear
+      </button>
+    </div>
+  )
+}
+
+export default TopBar
+
