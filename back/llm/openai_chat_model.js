aee68e5cb614803aeb824df47d017a5dbe811faf
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-18 15:29:56 +0100

    Filter only complete messages

--- back/llm/openai_chat_model.js
+++ back/llm/openai_chat_model.js
@@ -4,34 +4,6 @@ const fetch = require('node-fetch')
 const ChatCompletionModel = require('./chat_completion_model')
 const { OPENAI_API_KEY } = require('../config')
 
-const parseJSONDeltas = new Transform({
-  transform(chunk, encoding, callback) {
-    const messages = chunk.toString().split('\n\n')
-    for (const message of messages) {
-      const prefix = 'data: '
-      if (message.startsWith(prefix)) {
-        const withoutPrefix = message.substring(prefix.length)
-        if (withoutPrefix !== '[DONE]') {
-          const obj = JSON.parse(withoutPrefix)
-          this.push(obj)
-        }
-      }
-    }
-    callback()
-  },
-  objectMode: true
-})
-
-const convertToNativeJSON = new Transform({
-  transform(chunk, encoding, callback) {
-    if (chunk?.choices && chunk.choices[0]?.delta?.content) {
-      this.push({ status: 'completing', delta: chunk.choices[0].delta.content })
-    }
-    callback()
-  },
-  objectMode: true
-})
-
 class OpenAIChatModel extends ChatCompletionModel {
   constructor(config) {
     super()
@@ -45,9 +17,43 @@ class OpenAIChatModel extends ChatCompletionModel {
   async getCompletionStream(messages) {
     if (!messages) { throw 'completion requires at least one message' }
 
+    const parseJSONDeltas = new Transform({
+      transform(chunk, encoding, callback) {
+        const messages = chunk.toString().split('\n\n')
+        for (const message of messages) {
+          const prefix = 'data: '
+          if (message.startsWith(prefix)) {
+            const withoutPrefix = message.substring(prefix.length)
+            if (withoutPrefix !== '[DONE]') {
+              const obj = JSON.parse(withoutPrefix)
+              this.push(obj)
+            }
+          }
+        }
+        callback()
+      },
+      objectMode: true
+    })
+
+    const convertToNativeJSON = new Transform({
+      transform(chunk, encoding, callback) {
+        if (chunk?.choices && chunk.choices[0]?.delta?.content) {
+          this.push({ status: 'completing', delta: chunk.choices[0].delta.content })
+        }
+        callback()
+      },
+      objectMode: true
+    })
+
+    messages = messages
+      .filter(({ status }) => status === 'done')
+      .map(({ role, content }) => ({ role, content }))
+
+    console.log(messages)
+
     const request = {
       ...this.config,
-      messages: messages.map(({ role, content }) => ({ role, content })),
+      messages
     }
 
     const response = await fetch('https://api.openai.com/v1/chat/completions', {
