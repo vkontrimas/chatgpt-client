b5ae209045e0d1fc9bf3ef8da7b7252b1ba88688
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-19 13:43:08 +0100

    remove message slice

--- web/redux/store.js
+++ web/redux/store.js
@@ -1,9 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import message from './message'
 import user from './user'
 import chat from './chat_slice'
 
 export const store = configureStore({
-  reducer: { message, user, chat },
+  reducer: { user, chat },
   devTools: true,
 })
