acab3e01b543d93ca3983481f893717356c37478
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-19 11:54:25 +0100

    Insert commit hash into index.html

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
 run_cmd() {
-  echo "\$ $@"
+  echo $ "$@"
   if [ -z "$DRY_RUN" ]; then
     eval "$@"
   fi
 }
 
+update_index_html_hash() {
+  local hash=$(git --no-pager log --format=%h -n 1)
+  echo '> update index.html commit hash to' $hash
+  if [ -z "$DRY_RUN" ]; then
+    sed -i -e "s#\(<div id=\"root-version-tag\">\)latest#\1$hash#" web/index.html
+  fi
+}
+
+update_index_html_hash
 run_cmd cd 'web'
 # we need to install vite to build, hence the include
 run_cmd npm install --include dev
