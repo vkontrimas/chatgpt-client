0fe55a76414e38f8aec831937ef6d6f8e8d3057c
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-21 12:14:14 +0100

    Use separate component for chat list items

--- web/ui/ChatList.jsx
+++ web/ui/ChatList.jsx
@@ -1,13 +1,31 @@
 import '../css/ChatList.css'
 
 import axios from 'axios'
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useCallback } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams, useNavigate } from 'react-router-dom'
 
-import ChatTitleBar from './ChatTitleBar'
 import Loading from './Loading'
-import { setChats, setLoading } from '../redux/chat_slice'
+import { setChats, setLoading, destroyChat } from '../redux/chat_slice'
+
+const ChatListItem = ({ selected, id, title, handleDestroy, handleNavigate }) => (
+  <div className={`chat-list-item ${selected ? 'selected' : ''}`}>
+    <h3
+      className='chat-list-item-title'
+      onClick={handleNavigate}
+    >
+      {title}
+    </h3>
+      <button
+      className='button-clear chat-list-item-button'
+      onClick={handleDestroy}
+      aria-label='Delete chat'
+      >
+      <i className='fa fa-trash-o fa-2x'></i>
+      </button>
+  </div>
+)
+
 
 const ChatList = (props) => {
   const { map, loading } = useSelector(state => state.chat)
@@ -20,18 +38,39 @@ const ChatList = (props) => {
     const loadChats = async () => {
       dispatch(setLoading(true))
       const response = await axios.get('/api/chat', { headers: { Authorization: bearer } })
-      console.log(response.data)
       dispatch(setChats(response.data))
       dispatch(setLoading(false))
     }
     loadChats()
   }, [])
 
+  const handleDestroy = useCallback((id) => {
+    dispatch(destroyChat(id))
+    axios.delete(`/api/chat/${id}`, { headers: { Authorization: bearer }})
+    if (id === params.chatId) {
+      navigate('/', { replace: true })
+    }
+  }, [params.chatId, dispatch, navigate])
+
+  const handleNavigate = useCallback((id) => {
+    if (id !== params.chatId) {
+      navigate(`/chat/${id}`)
+    }
+  }, [params.chatId, navigate])
+
   const listItems = useMemo(() => { 
     return Object
       .values(map)
-      .map(({ id, title }) => (<ChatTitleBar selected={id === params.chatId} key={id} id={id} title={title}/>))
-  }, [map, params])
+      .map(({ id, title }) => (
+        <ChatListItem
+          selected={id === params.chatId}
+          key={id}
+          title={title}
+          handleDestroy={() => handleDestroy(id)}
+          handleNavigate={() => handleNavigate(id)}
+        />
+      ))
+  }, [map, params.chatId, destroyChat])
 
   return (
     <>
