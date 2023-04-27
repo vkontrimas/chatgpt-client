88fd9b2691db0152e16b9a14f0527bd49499b68f
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-27 12:43:27 +0100

    Remove devdb

--- devdb/create_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ../db/
-npm install
-npx sequelize-cli db:create
-NODE_ENV=test npx sequelize-cli db:create
--- devdb/migrate_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ../db
-npm install
-npx sequelize-cli db:migrate
-NODE_ENV=test npx sequelize-cli db:migrate
