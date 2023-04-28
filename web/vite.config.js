5a248511b7f1dda90ae3743da782a67729835608
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-28 17:16:46 +0100

    Refer to correct hostnames for docker

--- web/vite.config.js
+++ web/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: process.env.PORT || 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://backend:3001',
         changeOrigin: true,
       },
     },
