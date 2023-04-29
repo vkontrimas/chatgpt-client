65296a8903d4c22deeb188d2e245bf21df09b29d
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-04-29 01:15:51 +0100

    Restore backend functionality; stop serving static

--- back/app.js
+++ back/app.js
@@ -14,24 +14,19 @@ const path = require('path')
 const app = express()
 if (ENVIRONMENT !== 'test') {
   const morgan = require('morgan')
-  app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms ":referrer" ":user-agent"'))
+  app.use(morgan('short'))
 }
 if (ENVIRONMENT === 'development' || ENVIRONMENT === 'test') {
   const cors = require('cors')
   app.use(cors())
 }
-/*app.use(express.static('public'))
+
 app.use(express.json())
 app.use('/api/chat', userSession, chatRouter)
 app.use('/api/users', userSession, usersRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/register', registerRouter)
 app.use('/api/waitlist', waitlistRouter)
-app.get('*', (request, response) => response.sendFile(path.resolve('public', 'index.html')))
 app.use(unknownEndpoint)
 app.use(errorHandler)
-*/
-app.use((request, response) => {
-  response.status(404).end()
-})
 module.exports = app
