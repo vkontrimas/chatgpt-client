526795e9def10b2e611eb977874ba934369525e8
Vytautas Kontrimas <vytautas.kontrimas@outlook.com>
2023-05-02 14:02:53 +0100

    msgcontext: add handleOpenContext

--- web/ui/ChatMessageView.jsx
+++ web/ui/ChatMessageView.jsx
@@ -1,15 +1,19 @@
 import '../css/ChatMessageView.css'
 
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import ChatMessage from './ChatMessage'
+import { grabMessage } from '../redux/current_thread'
 
 const ChatMessageView = (props) => {
+  const dispatch = useDispatch()
   const shownMessages = useSelector(state => state.currentThread.shownMessages)
+
+  const handleOpenContext = (messageId) => dispatch(grabMessage(messageId))
 
   return (
     <div className='chat-message-view'>
-      {shownMessages.map(message => (<ChatMessage key={message.id} message={message} />))}
+      {shownMessages.map(message => (<ChatMessage key={message.id} message={message} handleOpenContext={handleOpenContext} />))}
     </div>
   )
 }
